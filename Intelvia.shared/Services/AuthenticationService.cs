4e02102b7eeee2bb1dd20a1781e5ca7a4678cb32
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-04-14 23:01:47 +0000

    Create AuthenticationService.cs
    there is explication in my note!!

---
+++ Intelvia.shared/Services/AuthenticationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AKSoftware.WebApi.Client;
+using Intelvia.shared.Models;
+
+namespace Intelvia.shared.Services
+{
+    public class AuthenticationService
+    {
+        private readonly string _baseUrl;
+
+        ServiceClient client = new ServiceClient();
+
+        public AuthenticationService(string url)
+        {
+            _baseUrl = url;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterRequest request)
+        {
+            var response = await client.PostAsync<UserManagerResponse>($"{_baseUrl}/api/auth/register", request);
+            return response.Result;
+        }
+    }
+}
