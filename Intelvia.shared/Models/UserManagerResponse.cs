85d6773e0645a2d5fee477c9d479df6d89f4495f
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-04-19 14:08:23 +0000

    create new Model in Models in Shared and add attribut n UserManagerResponse; Create Login compenentrazor in pages

--- Intelvia.shared/Models/UserManagerResponse.cs
+++ Intelvia.shared/Models/UserManagerResponse.cs
@@ -9,7 +9,7 @@ namespace Intelvia.shared.Models
         public string Message { get; set; }
         public bool IsSuccess { get; set; }
         public string[] Errors { get; set; }
-        //public Dictionary<string, string> UserInfo { get; set; }
+        public Dictionary<string, string> UserInfo { get; set; }
         public DateTime? ExpireDate { get; set; }
     }
 }
